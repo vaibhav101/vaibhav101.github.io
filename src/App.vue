06548b5b250d96aa776d13bbf0d5f4c68928cb38
Vaibhav <vaibhav.paliwal@outlook.com>
2022-09-17 23:18:26 +0100

    Create a blank sample to test

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,93 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="notification-header"></div>
+    <div class="header"></div>
+    <div class="body-container">
+      <div class="image-right"></div>
+      <div class="top-heading"></div>
+      <div class="body-text"></div>
+      <div class="call-to-action-container"></div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 0.2fr 0.2fr 2.6fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "notification-header"
+    "header"
+    "body-container";
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.notification-header {
+  grid-area: notification-header;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.header {
+  grid-area: header;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.body-container {
+  justify-self: center;
+  display: grid;
+  max-width: 1800px;
+  width: 100%;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "top-heading image-right"
+    "body-text image-right"
+    "call-to-action-container image-right";
+  grid-area: body-container;
+  transition: all 500ms;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.image-right {
+  grid-area: image-right;
+}
+
+.top-heading {
+  grid-area: top-heading;
+}
+
+.body-text {
+  grid-area: body-text;
+}
+
+.call-to-action-container {
+  grid-area: call-to-action-container;
+}
+
+.container {
+  height: 100%;
+  margin: 0;
+}
+
+/* For presentation only, no need to copy the code below */
+.container * {
+  border: 1px solid red;
+  position: relative;
+}
+
+.container *:after {
+  content: attr(class);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  justify-content: center;
 }
 </style>
